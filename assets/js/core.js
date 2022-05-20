a77a272a0ad97383dc2354c9eb97dc620c503161
Nikita Skvortsov <31357047+chetverolis@users.noreply.github.com>
2022-05-21 01:37:14 +0500

    Add files via upload

--- assets/js/core.js
+++ assets/js/core.js
@@ -2,8 +2,12 @@
 const flasks = document.querySelectorAll('div.flask'),
     hidden_flasks = document.querySelectorAll('div.hidden_flask');
 
-// console.log(flasks);
-// console.log(hidden_flasks);
+let ball = null,
+    b_color = null,
+    b_color_active = null,
+    click_check = 0;
+
+let collected = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Цвета
 const colors = [
@@ -58,15 +62,85 @@ function generateLvl() {
                 j -= 1;
             }
         }
-        // console.log(colors_check);
     }
 };
 
 generateLvl();
 
+function win() {
+    // Проверяем колбы на соответствие цветов, если цвета совпали, то заносим в массив 1
+    // Колбы
+    for (let i = 0; i < flasks.length; i++) {
+        // Шары
+        for (let j = 0; j < flasks[i].children.length; j++) {
+            // Проверяем на наличие шаров и их кол-ва
+            if (flasks[i].children[j] && flasks[i].children.length == 3) {
+                // Если первый шар равен второму и первый равен третьему, то заносим 1
+                if (flasks[i].children[0].getAttribute('b_color') == flasks[i].children[1].getAttribute('b_color') && flasks[i].children[0].getAttribute('b_color') == flasks[i].children[2].getAttribute('b_color')) {
+                    collected[i] = 1;
+                }
+            }
+        }
+    }
+
+    // Обнуляем проверку соответствия цветов
+    let collected_check = 0
+
+    // Проверяем после каждого хода, сколько колб заполнено верно
+    for (let i = 0; i < collected.length; i++) {
+        collected_check += collected[i]
+    }
+
+    // Если игрок собрал все 9, то он победил
+    if (collected_check == 9) {
+        setTimeout(function() {
+            alert('You Win!');
+        }, 1000)
+    }
+}
+
 for (let flask of flasks) {
     flask.addEventListener('click', function() {
-        flask.children[0].style.position = 'absolute';
-        flask.children[0].style.top = '-54px';
+        if (click_check == 0) {
+            // Доставание шарика из колбы
+            let active_ball = document.querySelector('div.active_ball'),
+                ball_check = typeof(active_ball) != "undefined" && active_ball !== null;
+            if (ball_check != true) {
+                ball = flask.children[0];
+                b_color_active = ball.getAttribute('b_color');
+                flask.children[0].classList.add('active_ball');
+                flask.children[0].style.position = 'absolute';
+                flask.children[0].style.bottom = `${flask.children[0].clientHeight * 3 + 12}px`;
+                click_check = 1;
+            }
+        } else {
+            // Добавление шарика в колбу
+            if (flask.children.length >= 1) {
+                b_color = flask.children[0].getAttribute('b_color');
+            }
+            // Если шарик проходит проверку, то мы перекидываем его
+            if ((b_color_active == b_color && flask.children.length != 3) || flask.children.length == 0) {
+                flask.prepend(ball);
+                setTimeout(function() {
+                    flask.children[0].style.bottom = `${(flask.children[0].clientHeight * (flask.children.length - 1)) + ((flask.children.length - 1) * 4)}px`;
+                }, 300)
+                ball.classList.remove('active_ball');
+                ball = null,
+                b_color = null,
+                b_color_active = null;
+                click_check = 0;
+            } else {
+                // Иначе возвращаем обратно в его колбу
+                ball = document.querySelector('div.active_ball');
+                ball.classList.remove('active_ball');
+                ball.style.position = null;
+                ball.style.bottom = null;
+                ball = null,
+                b_color = null,
+                b_color_active = null;
+                click_check = 0;
+            }
+        }
+        win();
     })
 }
